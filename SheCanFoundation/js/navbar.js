d1c2235482a5549f0ccb746b5b7899a946452a89
Monisha <88149825+Monisha228@users.noreply.github.com>
2026-05-29 13:28:16 +0530

    Delete SheCanFoundation directory

--- SheCanFoundation/js/navbar.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navbar = document.querySelector(".nav-items");
-
-hamburger.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
